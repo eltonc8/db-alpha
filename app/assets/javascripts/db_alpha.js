1d07967451f1f7b18fe66eaf95ac9f175dbe2b96
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-11 17:31:24 -0700

    complete static pages

---
+++ app/assets/javascripts/db_alpha.js
@@ -0,0 +1,12 @@
+window.DbAlpha = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  DbAlpha.initialize();
+});
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def root
     if signed_in?
-      render :landing_page
-    else
       render :index
+    else
+      render :landing_page
     end
   end
 end
