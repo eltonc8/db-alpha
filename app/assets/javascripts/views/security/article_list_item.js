fc49956b27b53e71ffd2243bf5d93ba187ce0d51
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-13 20:06:06 -0700

    complete core securities show with styling

--- app/assets/javascripts/views/security/article_list_item.js
+++ app/assets/javascripts/views/security/article_list_item.js
@@ -1,6 +1,7 @@
 DbAlpha.Views.ArticleListItem = Backbone.View.extend({
   template: JST['security/article_list_item'],
   tagName: "li",
+  className: "article-list-item",
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
