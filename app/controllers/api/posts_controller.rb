0adb67959d45c5459eefb84498adf87af4a46368
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-17 16:21:51 -0700

    complete some styling of post index

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -21,8 +21,8 @@ class Api::PostsController < ApplicationController
 
   def update
     @post = current_user.posts.find(params[:id])
-
-    if @post && @post.save(post_params)
+    debugger
+    if @post && @post.update(post_params)
       render json: @post
     else
       render json: @post.errors.full_messages, status: 422
@@ -37,8 +37,8 @@ class Api::PostsController < ApplicationController
 
   private
   def post_params
-    post_params = params.require(:post).permit(:user_id, :title, :tag, :note, :body)
-    post_params.shared_with ||= []
+    post_params = params.require(:post).permit(:user_id, :title, :tag, :note, :body, :share_with)
+    post_params[:share_with] ||= []
     post_params
   end
 end
