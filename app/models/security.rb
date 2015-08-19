29eddda3009b8e4406a89513da36e0c279e00230
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-19 10:55:46 -0700

    fix regexp for securities name

--- app/models/security.rb
+++ app/models/security.rb
@@ -4,18 +4,25 @@ class Security < ActiveRecord::Base
 
   def self.search_or_initialize(value)
     begin
-      result = value.to_i > 0 ? self.find(value) : self.find_or_initialize_by(symbol: value.upcase)
+      result = value.to_i > 0 ?
+               self.find(value) :
+               self.find_or_initialize_by(symbol: value.upcase)
     rescue
       result = nil
     end
 
     if result
+      #Cannot be refactored into instance method, as Feedjira cannot be used in an instance method yet.
       result.feeds = Feedjira::Feed.fetch_and_parse("http://feeds.finance.yahoo.com/rss/2.0/headline?region=US&s=#{result.symbol.html_safe}")
-      /for (?<name>.*)\Z/ =~ result.feeds.description
+
+      /for (?<name>.*)\Z/ =~ result.feeds.description #grabs the name out of the feed
+      name.slice!(/ (common|comm|new com)\b.*\Z/i)
+
       if name
-        result.name ||= name
+        result.name ||= name.dup
         google_web = Google::Search::Web.new(query: name + " investor")
         result.website ||= google_web.first.visible_uri
+        name.slice!(/ (inc|corp)\.+.*\Z/i)
         google_image = Google::Search::Image.new(query: name + " logo")
         result.image ||= google_image.first.uri
       end
