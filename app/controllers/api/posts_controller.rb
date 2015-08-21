ae572fa95029200cb96cabf8d788bea85d9b1985
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-21 10:27:49 -0700

    add requirement to api interface

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,43 +1,45 @@
-class Api::PostsController < ApplicationController
-  def index
-    @posts = Post.search( id: current_user.id, security_id: params[:security_id] )
-    render "posts/index.json"
-  end
-
-  def create
-    @post = current_user.posts.new(post_params)
-    if @post.save
-      render json: @post
-    else
-      render json: @post.errors.full_messages, status: 422
+module Api
+  class PostsController < ApiController
+    def index
+      @posts = Post.search( id: current_user.id, security_id: params[:security_id] )
+      render "posts/index.json"
     end
-  end
 
-  def show
-    @post = Post.find(params[:id])
-    render json: @post
-  end
-
-  def update
-    @post = current_user.posts.find(params[:id])
-
-    if @post && @post.update(post_params)
-      render json: @post
-    else
-      render json: @post.errors.full_messages, status: 422
+    def create
+      @post = current_user.posts.new(post_params)
+      if @post.save
+        render json: @post
+      else
+        render json: @post.errors.full_messages, status: 422
+      end
     end
-  end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.try(:destroy)
-    render json: {};
-  end
+    def show
+      @post = Post.find(params[:id])
+      render json: @post
+    end
 
-  private
-  def post_params
-    post_params = params.require(:post).permit(:user_id, :title, :tags, :note, :body, :shared_with)
-    post_params[:shared_with] ||= []
-    post_params
+    def update
+      @post = current_user.posts.find(params[:id])
+
+      if @post && @post.update(post_params)
+        render json: @post
+      else
+        render json: @post.errors.full_messages, status: 422
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.try(:destroy)
+      render json: {};
+    end
+
+    private
+    def post_params
+      post_params = params.require(:post).permit(:user_id, :title, :tags, :note, :body, :shared_with)
+      post_params[:shared_with] ||= []
+      post_params
+    end
   end
 end
--- app/controllers/api/securities_controller.rb
+++ app/controllers/api/securities_controller.rb
@@ -1,18 +1,20 @@
-class Api::SecuritiesController < ApplicationController
-  def create
-  end
-
-  def show
-    @security = Security.search_or_initialize(params[:id])
-    if @security
-      render "securities/show.json"
-    else
-      render JSON: @security, status: 404
+module Api
+  class SecuritiesController < ApiController
+    def create
     end
-  end
 
-  private
-  def security_params
-    params.require(:security).permit(:symbol, :id, :name, :website)
+    def show
+      @security = Security.search_or_initialize(params[:id])
+      if @security
+        render "securities/show.json"
+      else
+        render JSON: @security, status: 404
+      end
+    end
+
+    private
+    def security_params
+      params.require(:security).permit(:symbol, :id, :name, :website)
+    end
   end
 end
