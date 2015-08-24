0dcd2005a3815aa29dade937b846f22406d3d925
eltonc88 <eltonchan@cal.berkeley.edu>
2015-08-23 21:16:49 -0700

    complete gradient on percent change div

--- app/assets/javascripts/views/market/market_board_item.js
+++ app/assets/javascripts/views/market/market_board_item.js
@@ -4,7 +4,8 @@ DbAlpha.Views.MarketBoardItem = Backbone.View.extend({
   className: "board-item",
 
   initialize: function () {
-    this.listenTo( this.model, "sync", this.render );
+    this._percent = 0;
+    this.listenTo( this.model, "sync change", this.render );
     this.listenTo( this.model.quotes(), "sync change", this._update );
     this.render();
   },
@@ -12,17 +13,34 @@ DbAlpha.Views.MarketBoardItem = Backbone.View.extend({
   render: function () {
     var content = this.template({model: this.model});
     this.$el.html( content );
-    this._update();
+    this.$(".percent-change").attr("id", this.model.escape("symbol"));
+    this._update(1000);
     return this;
   },
 
   update: function () {
     if (!this.model.quotes().get("results")) return;
     var quote = this.model.quotes().get("results").quote;
-    this.$("div.percent-change").html( _.escape(quote.PercentChange) );
     this.$("div.change").html( _.escape(quote.Change) );
     this.$("div.last-trade").html( _.escape(quote.LastTradePriceOnly) );
+
+    var percent = +/-?\d*\.\d*/.exec( quote.PercentChange );
+    percent = Math.round( percent * 100) / 100;
+    if      (this._percent < percent) { this._triggerGreen(); }
+    else if (this._percent > percent) { this._triggerRed(); }
+
+    var g = Math.round( Math.atan(percent) * this._factor + 127.5);
+    var r = 255 - g;
+    var b = Math.min(r,g);
+    var colors = [r,g,b];
+    var gradient = this._gradient.replace(/#{}/g, colors.join(","));
+    this.$(".percent-change").css("background", gradient);
+    this.$("div.percent-change").html( percent + "%" );
+    this._percent = percent;
   },
+
+  _factor: 255 / Math.PI,
+  _gradient: "linear-gradient(to right, rgba(0,0,0,0) 0%,rgba(#{},1) 20%,rgba(#{},1) 80%,rgba(0,0,0,0) 100%)",
 
   _triggerDefault: function () {
     this.$el.removeClass("red").removeClass("green");
@@ -38,7 +56,7 @@ DbAlpha.Views.MarketBoardItem = Backbone.View.extend({
     setTimeout(this._triggerDefault.bind(this), 200);
   },
 
-  _update: function () {
-    setTimeout(this.update.bind(this), Math.random() * 8000);
+  _update: function (option) {
+    setTimeout(this.update.bind(this), Math.random() * (option || 8000));
   }
 });
