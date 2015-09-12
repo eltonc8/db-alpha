2da01d856a1680372465fb31e8cd6d4d9c422f3b
eltonc88 <eltonchan@cal.berkeley.edu>
2015-09-11 23:48:08 -0700

    add update info to market view

--- app/assets/javascripts/views/market/market_time.js
+++ app/assets/javascripts/views/market/market_time.js
@@ -1,18 +1,40 @@
 DbAlpha.Views.MarketTime = Backbone.View.extend({
   template: JST['market/market_time'],
+  ops: {},
 
   initialize: function () {
     this.interval = setInterval(this.render.bind(this), 1000);
+    this.setMarketTimes();
+  },
+
+  getCountdown: function () {
+    return;
   },
 
   render: function () {
-    var time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
-    if (this.tick = !this.tick) time = time.replace(/:/g, " ");
-    var content = this.template({
-      currentTime: time
-    });
+    this.updateOps();
+    var content = this.template( this.ops );
     this.$el.html( content );
+
     return this;
+  },
+
+  setMarketTimes: function () {
+    if (DbAlpha.isMarketOpen()) {
+      this.ops.nextClose = DbAlpha.nextMarketClose();
+      this.ops.nextOpen = 0;
+    } else {
+      this.ops.nextClose = 0;
+      this.ops.nextOpen = DbAlpha.nextMarketOpen();
+    }
+  },
+
+  updateOps: function () {
+    var time = new Date(), ops = this.ops;
+    ops.localTime = time.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    if (ops.localTime[7] % 2) ops.localTime = ops.localTime.replace(/:/g, " ");
+
+    if (time > (ops.nextOpen || ops.nextClose)) this.setMarketTimes();
   },
 
   remove: function () {
