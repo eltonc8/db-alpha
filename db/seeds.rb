eb83df682a1e380568911f4820c45a5c8fc68d53
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-22 15:22:23 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ Security.create!(symbol: "aapl", twitter_widget_id: 631601971376447488)
 Security.create!(symbol: "spy", twitter_widget_id: 634758732908490754)
 Security.create!(symbol: "goog", twitter_widget_id: 634761785019625472)
 Security.create!(symbol: "fb", twitter_widget_id: 634762188230684672)
+Security.create!(symbol: "grpn", twitter_widget_id: 668519773295280128)
 
 Post.create!(user_id: 2, title: "S&P 500 falls to support level", shared_with: "", tags: "SPY",  body:"\N  The S&P500 index falls below 200dmv today.\r\n\r\n![aug 20](https://dwq4do82y8xi7.cloudfront.net/x/fv3fezKr/)\r\n\r\n2040 appears to be a support level in the past. **Will it break below this support level in the short term?! **Fear was all over the market today.\r\n\r\nIf I had capital aiming for a long term entry, I may consider this a good time.",  created_at:  "2015-08-20 20:50:46.798332")
 Post.create!(user_id: 2, title: "S&P 500 fell through 2000. at another support level 1970", shared_with: "", tags: "SPY", body:  "\N  But perhaps even that will not hold. aim for 1880?\r\n\r\n![1880 target](https://dwq4do82y8xi7.cloudfront.net/x/3n2cHbAY/)", created_at:  "2015-08-21 20:43:17.577462")
